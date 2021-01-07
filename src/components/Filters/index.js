8b8fff19ceec23877f121aa7e6e188e81a75914f
nikolafullstack <nikola.todor701@gmail.com>
2021-01-07 19:09:47 +0800

    update filter panel responsive ui

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -29,31 +29,33 @@ const Filters = () => {
   return (
     <div className='container-fluid'>
       <div className='row my-5'>
-        <div className='col-lg-6 col-12 d-flex'>
-          {shapeFiltersList.map((sType, index) => (
-            <div
-              className={classNames('shape-filter text-uppercase font-italic font-weight-bold mr-4', {
-                'text-dark': selectedShapeFilters.indexOf(sType) !== -1,
-                'text-muted': selectedShapeFilters.indexOf(sType) === -1
-              })}
-              key={`shape-filter-${index}`}
-              onClick={() => handleShapeFilter(sType)}
-            >
-              <h4>{sType}</h4>
-            </div>
-          ))}
-        </div>
-        <div className='col-lg-6 col-12 d-flex justify-content-end'>
-          {colorFiltersList.map((cType, index) => (
-            <div
-              className={classNames('color-filter ml-3', {
-                'active': selectedColorFilters.indexOf(cType) !== -1
-              })}
-              key={`color-filter-${index}`}
-              style={{backgroundColor: cType}}
-              onClick={() => handleColorFilter(cType)}
-            />
-          ))}
+        <div className='col-12 d-flex justify-content-between flex-wrap'>
+          <div className='d-flex flex-wrap'>
+            {shapeFiltersList.map((sType, index) => (
+              <div
+                className={classNames('shape-filter text-uppercase font-italic font-weight-bold mr-4 mb-3', {
+                  'text-dark': selectedShapeFilters.indexOf(sType) !== -1,
+                  'text-muted': selectedShapeFilters.indexOf(sType) === -1
+                })}
+                key={`shape-filter-${index}`}
+                onClick={() => handleShapeFilter(sType)}
+              >
+                <h4>{sType}</h4>
+              </div>
+            ))}
+          </div>
+          <div className='d-flex flex-wrap'>
+            {colorFiltersList.map((cType, index) => (
+              <div
+                className={classNames('color-filter mr-3 mb-3', {
+                  'active': selectedColorFilters.indexOf(cType) !== -1
+                })}
+                key={`color-filter-${index}`}
+                style={{backgroundColor: cType}}
+                onClick={() => handleColorFilter(cType)}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
